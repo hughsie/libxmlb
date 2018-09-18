23142e118be4be622a17f469fc5a73d337e317cf
Richard Hughes <richard@hughsie.com>
2018-09-18 19:50:59 +0100

    Add initial build scaffolding

---
+++ src/xmlb.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2015 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+/**
+ * SECTION:libxmlb
+ * @short_description: Helper objects for accessing libxmlb
+ */
+
+#ifndef __LIBXMLB_H__
+#define __LIBXMLB_H__
+
+#define __LIBXMLB_H_INSIDE__
+
+#include <libxmlb/libxmlb-client.h>
+#include <libxmlb/libxmlb-common.h>
+#include <libxmlb/libxmlb-device.h>
+#include <libxmlb/libxmlb-enums.h>
+#include <libxmlb/libxmlb-error.h>
+#include <libxmlb/libxmlb-release.h>
+#include <libxmlb/libxmlb-remote.h>
+#include <libxmlb/libxmlb-version.h>
+
+#ifndef LIBXMLB_DISABLE_DEPRECATED
+#include <libxmlb/libxmlb-deprecated.h>
+#endif
+
+#undef __LIBXMLB_H_INSIDE__
+
+#endif /* __LIBXMLB_H__ */
+
