d5d07a6fb573c5d1156a1d70c95258fef76dbab7
Richard Hughes <richard@hughsie.com>
2018-10-10 12:45:27 +0100

    Enable coverage scanning in CI

--- contrib/ci/build_and_test.sh
+++ contrib/ci/build_and_test.sh
@@ -4,9 +4,12 @@ export LC_ALL=C.UTF-8
 mkdir -p build && cd build
 rm * -rf
 meson .. \
+    -Db_coverage=true \
     -Dgtkdoc=true \
     -Dtests=true $@
 ninja -v || bash
 ninja test -v
+ninja coverage-text
+cat meson-logs/coverage.txt
 DESTDIR=/tmp/install-ninja ninja install
 cd ..
