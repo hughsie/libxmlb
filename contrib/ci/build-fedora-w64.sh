37887fd02f2062ca3ca5c0846c51ae32ed835af9
Richard Hughes <richard@hughsie.com>
2019-11-21 08:29:39 +0000

    Add MinGW Windows 64 builds to CI

---
+++ contrib/ci/build-fedora-w64.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+export LC_ALL=C.UTF-8
+mkdir -p build && cd build
+rm * -rf
+meson .. \
+    --cross-file=../contrib/mingw64.cross \
+    -Dintrospection=false \
+    -Dgtkdoc=false \
+    -Dtests=true $@
+ninja -v || bash
+wine reg add "HKEY_CURRENT_USER\Environment" /v PATH /d /usr/x86_64-w64-mingw32/sys-root/mingw/bin
+ninja test -v
+DESTDIR=/tmp/install-ninja ninja install
+cd ..
