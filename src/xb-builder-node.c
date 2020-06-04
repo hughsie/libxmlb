hughsie/libxmlb

commit b7f2509c2b322d9e4e4d808e746b6060cc32220b

Do not show a critical warning for invalid XML
If we ignore a node due to XB_BUILDER_COMPILE_FLAG_SINGLE_LANG then also ignore
the child nodes. This ensures we do not warn when presented with something like:

    <description xml:lang=de>
      <ul><li xml:lang=de></li><li></li></ul>
    </description>
    <description>
      <ul><li></li></ul>
    </description>

Fixes https://github.com/hughsie/libxmlb/issues/50

src/xb-builder-node.c
@@ -785,6 +785,10 @@ xb_builder_node_set_priority (XbBuilderNode *self, gint priority)
 	XbBuilderNodePrivate *priv = GET_PRIVATE (self);
 	g_return_if_fail (XB_IS_BUILDER_NODE (self));
 	priv->priority = priority;
+	for (guint i = 0; i < priv->children->len; i++) {
+		XbBuilderNode *c = g_ptr_array_index (priv->children, i);
+		xb_builder_node_set_priority (c, priority);
+	}
 }
 
 /* private */

commit d88367008cba03bbaefc918ab126f48b1669a292

xb-builder-node: Don’t crash if priv->children is NULL
Since commit d12765d0, `priv->children` is loaded lazily, and can be
`NULL`. Commit b7f2509c2b3 (#50) introduced a crash in that case.

Signed-off-by: Philip Withnall <withnall@endlessm.com>

src/xb-builder-node.c
@@ -785,7 +785,7 @@ xb_builder_node_set_priority (XbBuilderNode *self, gint priority)
 	XbBuilderNodePrivate *priv = GET_PRIVATE (self);
 	g_return_if_fail (XB_IS_BUILDER_NODE (self));
 	priv->priority = priority;
-	for (guint i = 0; i < priv->children->len; i++) {
+	for (guint i = 0; priv->children != NULL && i < priv->children->len; i++) {
 		XbBuilderNode *c = g_ptr_array_index (priv->children, i);
 		xb_builder_node_set_priority (c, priority);
 	}