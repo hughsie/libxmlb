8c5d59d337e09b6fd90c24a788690110c2c3594a
Philip Withnall <pwithnall@endlessos.org>
2021-01-12 10:45:18 +0000

    xb-query-context: Only use G_ALIGNOF if GLib is new enough
    Unfortunately it’s not protected by `GLIB_VERSION_MAX_ALLOWED` because
    we couldn’t get that working for macros in public headers in GLib.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

--- src/xb-query-context.c
+++ src/xb-query-context.c
@@ -33,7 +33,9 @@ typedef struct {
 } RealQueryContext;
 
 G_STATIC_ASSERT (sizeof (XbQueryContext) == sizeof (RealQueryContext));
+#if GLIB_CHECK_VERSION(2, 60, 0)
 G_STATIC_ASSERT (G_ALIGNOF (XbQueryContext) == G_ALIGNOF (RealQueryContext));
+#endif
 
 G_DEFINE_BOXED_TYPE (XbQueryContext, xb_query_context,
 		     xb_query_context_copy, xb_query_context_free)
