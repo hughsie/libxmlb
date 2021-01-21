c756e6a8f4b59ac84201839b5afae388007c5c97
Richard Hughes <richard@hughsie.com>
2021-01-21 10:45:02 +0000

    trivial: Show UTF-8 when running the self tests

--- src/xb-self-test.c
+++ src/xb-self-test.c
@@ -7,6 +7,7 @@
 #include "config.h"
 
 #include <gio/gio.h>
+#include <locale.h>
 
 #include "xb-builder.h"
 #include "xb-builder-node.h"
@@ -2482,6 +2483,8 @@ main (int argc, char **argv)
 	/* only critical and error are fatal */
 	g_log_set_fatal_mask (NULL, G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL);
 	g_setenv ("G_MESSAGES_DEBUG", "all", TRUE);
+
+	setlocale (LC_ALL, "");
 
 	/* tests go here */
 	g_test_add_func ("/libxmlb/common", xb_common_func);
