e45db2b0657a9ca24728148f4fee6b971bfdc5ed
Richard Hughes <richard@hughsie.com>
2022-01-12 15:51:47 +0000

    Show the value bindings when using XB_SILO_PROFILE_FLAG_XPATH
    Otherwise, with lots of different bound values the output is somewhat
    meaningless...

---
+++ src/xb-value-bindings-private.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2022 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include <glib.h>
+
+gchar *
+xb_value_bindings_to_string(XbValueBindings *self);
