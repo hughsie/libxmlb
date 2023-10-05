1257bca32f46c75e3fcc60787aae920f5059dc49
peigongdsd <51317171+peigongdsd@users.noreply.github.com>
2023-10-05 16:39:39 +0800

    Update src/xb-builder-source.c
    Co-authored-by: Philip Withnall <53172+pwithnall@users.noreply.github.com>

--- src/xb-builder-source.c
+++ src/xb-builder-source.c
@@ -467,6 +467,8 @@ xb_builder_source_get_istream(XbBuilderSource *self, GCancellable *cancellable, 
 			return NULL;
 		if (g_strcmp0(content_type, "application/xml") == 0)
 			break;
+		/* Also accept the text/xml alias, just in case the user’s content-type database is slightly broken
+		 * (application/xml should normally be what’s used): */
 		if (g_strcmp0(content_type, "text/xml") == 0)
 			break;
 
